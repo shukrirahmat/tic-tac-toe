4f41a74a6731aa2c34fe6f8d109b8f8b4c0458f2
Shukri Rahmat <119567950+shukrirahmat@users.noreply.github.com>
2024-04-12 21:21:29 +0800

    Got the game to play correcly
    no restart yet, no aesthetics

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function Gameboard() {
 
     let board = [];
+    let turn = 0;
 
     const reset = () => {
         board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+        turn = 0;
     };
 
     const getBoard = () => board;
@@ -13,6 +15,9 @@ function Gameboard() {
     const setSquare = (index, mark) => {
         board[index] = mark;
     }
+
+    const getTurn = () => turn;
+    const advanceTurn = () => turn++;
 
     const checkWin = (mark) => {
         if (board[0] === mark && board[1] === mark && board[2] === mark) {
@@ -48,73 +53,99 @@ function Gameboard() {
         }
     }
 
+    /*
     const display = () => {
         console.log(board[0] + "  " + board[1] + "  " + board[2]);
         console.log(board[3] + "  " + board[4] + "  " + board[5]);
         console.log(board[6] + "  " + board[7] + "  " + board[8]);
     }
+    */
 
-    return { reset, getBoard, getSquare, setSquare, checkWinner, display};
+    return { reset, getBoard, getSquare, setSquare, checkWinner, getTurn, advanceTurn};
 }
 
 function Player(name, mark) {
 
-    const chooseSquare = () => {
-        return prompt(mark + " enter square");
+    const chooseSquare = (board, index) => {
+        if (board.getSquare(index) === " ") {
+            board.setSquare(index, mark);
+            board.advanceTurn();
+            display.updateBoard(board.getBoard());
+        } 
     }
 
     return {name, mark, chooseSquare};
 }
+
+const display = (function() {
+
+    const squares = document.querySelectorAll('.square');
+    const footer = document.querySelector('.footer');
+
+    const getSquareGrids = () => squares;
+    const getFooter = () => footer;
+
+    const updateBoard = (board) => {
+        squares.forEach(
+            function(node, index) {
+                node.textContent = board[index];
+            }
+        );
+    }; 
+
+    return {updateBoard, getSquareGrids, getFooter};
+
+})();
 
 function Game() {
 
     const gameboard = Gameboard();
     const player1 = Player("one", "X");
     const player2 = Player("two", "O");
-    let turn = 0;
 
-    const runTurn = (turnPlayer) => {
-        while (true) {
-            let squareIndex = turnPlayer.chooseSquare();
-            if (gameboard.getSquare(squareIndex) === " " ) {
-                gameboard.setSquare(squareIndex, turnPlayer.mark);
-                break;
-            } else {
-                console.log("square taken");
-            }
-        }
-    }
 
-    const run = () => {
+
+    const start = () => {
         
         gameboard.reset();
-        turn = 0;
+        display.updateBoard(gameboard);
 
-        while (gameboard.checkWinner() == null) {
-            if (turn % 2 == 0) {
-                runTurn(player1);
-            } else {
-                runTurn(player2)
+        display.getSquareGrids().forEach(
+            function(node, index) {
+                node.addEventListener('click', function() {
+                    if (gameboard.checkWinner() !== null) {
+                        //do nothing
+                    } else if (gameboard.getTurn() % 2 === 0) {
+                        player1.chooseSquare(gameboard, index);
+                    } else {
+                        player2.chooseSquare(gameboard, index);
+                    };
+
+                    if (gameboard.checkWinner() !== null) {
+                        end();
+                    }
+
+                })
             }
-            gameboard.display();
-            turn++;
-        }
-        const winner = gameboard.checkWinner();
-        if (winner === "draw") {
-            console.log("it's a draw");
-        } else {
-            if (player1.mark === winner) {
-                console.log(player1.name + " wins");
-            } else {
-                console.log(player2.name + " wins");
-            }
-        }
+        );
     }
 
-    return {run};
+    const end = () => {
+        let winner = gameboard.checkWinner();
+        if (winner === 'draw') {
+            display.getFooter().textContent = "It's a draw!";
+        }  else {
+            if (winner === player1.mark) {
+                display.getFooter().textContent = player1.name + " wins!";
+            } else {
+                display.getFooter().textContent = player2.name + " wins!";
+            }
+        }       
+    }
+
+    return {start};
 
 }
 
 const game = Game();
-
-game.run();
+game.start();
